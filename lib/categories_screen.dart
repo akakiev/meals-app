8733ee931c4e49feb553d8f1724ee852a74e07b4
Serhii Baraban <akakieva@gmail.com>
2023-02-08 10:55:43 +0100

    first commit for Meals App

---
+++ lib/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
