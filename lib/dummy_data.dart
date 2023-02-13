d9448fe1f326f3b7e38f6419a207792a58dc6099
Serhii Baraban <akakieva@gmail.com>
2023-02-13 10:28:07 +0100

    finished the meal model

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import './models/category.dart';
 import './models/meal.dart';
 
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,3 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+enum Complexity {
+  Simple,
+  Challenging,
+  Hard,
+}
+
+enum Affordability {
+  Affordable,
+  Pricey,
+  Luxurious,
+}
+
 class Meal {
   final String id;
   final List<String> categories;
@@ -8,10 +22,24 @@ class Meal {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-  bool isGlutenFree;
-  bool isVegan;
-  bool isVegetarian;
-  bool isLactoseFree;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
 
-  Meal({this.affordability, this.id, this.categories, })
+  const Meal({
+    @required this.id,
+    @required this.categories,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.ingredients,
+    @required this.steps,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+    @required this.isGlutenFree,
+    @required this.isLactoseFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
+  });
 }
