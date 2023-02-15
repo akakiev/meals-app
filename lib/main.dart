8d457a2431ff7ec237b1cffbf0d638292bc2c9a7
Serhii Baraban <akakieva@gmail.com>
2023-02-15 17:43:01 +0100

    added the meal detail page and the navigating to it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (ctx) => CategoriesScreen(),
           CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
         });
   }
 }
